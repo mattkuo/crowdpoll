0917ca99eece10b813abbdadc4010052f13711ac
Matthew Kuo <matthewkuo92@gmail.com>
2015-07-30 15:45:36 -0700

    Ready for development

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('crowdpollApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, Auth) {
     $scope.awesomeThings = [];
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
