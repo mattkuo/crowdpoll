3f6572f0ec5396bf9ac79dd649ac752b0631f6fd
Matthew Kuo <matthewkuo92@gmail.com>
2015-11-11 04:44:55 -0800

    Voting works properly

--- client/app/showPoll/showPoll.controller.js
+++ client/app/showPoll/showPoll.controller.js
@@ -1,49 +1,65 @@
 'use strict';
 
 angular.module('crowdpollApp')
-  .controller('ShowPollCtrl', function ($scope, $http, $routeParams, Auth) {
-    // $scope.labels = [];
-    $scope.data = {
-        optionNames: [],
-        votes: [],
-        selectedOptions: ''
-    };
+  .controller('ShowPollCtrl', function ($scope, $http, $routeParams, Auth, alertFactory) {
 
-    $scope.poll = {};
+  $scope.data = {
+    optionNames: [],
+    votes: [],
+    selectedOptions: '',
+    hasAlreadyVoted: false
+  };
 
-    $scope.submitVote = function() {
-      var newPoll = $scope.poll;
+  $scope.poll = {};
 
-      newPoll.fields.forEach(function(current) {
-        if (current._id === $scope.data.selectedOptions) {
-            var userId = Auth.getCurrentUser()._id;
-            current.votes.push(userId);
-        }
-      });
+  $scope.submitVote = function() {
+    var newPoll = $scope.poll;
 
-      $http.put('/api/polls/' + newPoll._id, newPoll).then(function(data) {
-        if (data.status !== 200) { return console.log('Error submiting poll'); }
-
-        console.log(data);
-      });
-    };
-
-    $http.get('/api/polls/' + $routeParams.id).then(function(data) {
-
-      if (data.status !== 200) {
-        return console.log('unable to get poll data.');
+    newPoll.fields.forEach(function(current) {
+      if (current._id === $scope.data.selectedOptions) {
+        var userId = Auth.getCurrentUser()._id;
+        current.votes.push(userId);
       }
-
-      var poll = data.data;
-      $scope.poll = poll;
-
-      poll.fields.forEach(function(field) {
-        $scope.data.optionNames.push(field.optionName);
-        $scope.data.votes.push(field.votes.length);
-      });
-
-    }, function(err) {
-      console.log(err);
     });
 
+    $http.put('/api/polls/' + newPoll._id, newPoll).then(function(data) {
+      if (data.status !== 200) { return console.log('Error submiting poll'); }
+      alertFactory.add('Your answer was submitted successfully.', 'success');
+      $scope.data.hasAlreadyVoted = true;
+      fillChartOptions();
+    });
+  };
+
+  $http.get('/api/polls/' + $routeParams.id).then(function(data) {
+
+    if (data.status !== 200) {
+      return alertFactory.add('Unable to get polldata.', 'danger');
+    }
+    var poll = data.data;
+    $scope.poll = poll;
+
+    if (hasUserVoted()) {
+      $scope.data.hasAlreadyVoted = true;
+      fillChartOptions();
+    }
+
+  }, function(err) {
+    console.log(err);
   });
+
+  function fillChartOptions() {
+    $scope.poll.fields.forEach(function(field) {
+      $scope.data.optionNames.push(field.optionName);
+      $scope.data.votes.push(field.votes.length);
+    });
+  }
+
+  function hasUserVoted() {
+    return $scope.poll.fields.some(function(field) {
+      return field.votes.some(function(userId) {
+        return Auth.getCurrentUser()._id === userId;
+      });
+    });
+  }
+
+});
